d0fa244f0dcf7cf4154a39b95d1c65debcbfc85f
kws02352 <47688611+kws02352@users.noreply.github.com>
2021-09-14 23:28:19 +0900

    Update train.py

--- UNet/train.py
+++ UNet/train.py
@@ -87,7 +87,7 @@ class UNet(nn.module):
         self.dec1_2 = CBR2d(in_channels=2*64, out_channels=64)
         self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
 
-        self.fc = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1, padding=0, bias=True)
+        self.fc = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
 
     def forward(self, x):
         enc1_1 = self.enc1_1(x)
@@ -133,3 +133,86 @@ class UNet(nn.module):
         x = self.fc(dec1_1)
 
         return x
+
+## 데이터 로더 구현
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transforms = transforms
+
+        lst_data = os.listdir(self.data_dir)
+
+        lst_label = [f for f in lst_data if f.startswith('label')]
+        lst_input = [f for f in lst_data if f.startswith('input')]
+
+        lst_label.sort()
+        lst_input.sort()
+
+        self.lst_label = lst_label
+        self.lst_input = lst_input
+
+    def __len__(self):
+        return len(self.lst_label)
+
+    def __getitem__(self, index):
+        label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
+        input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
+
+        label = label/255.0
+        input = input/255.0
+
+        if label.ndim == 2:
+            label = label[:, :, np.newaxis]
+        if input.ndim == 2:
+            input = input[:, :, np.newaxis]
+        
+        data = {'input': input, 'label': label}
+
+        if self.transform:
+            data = self.transform(data)
+        
+        return data
+
+## transform 구현하기
+class ToTensor(object):
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        label = label.transpose((2, 0, 1)).astype(np.float32)
+        input = input.transpose((2, 0, 1)).astype(np.float32)
+
+        data = {'label': torch.from_numpy(label), 'input': torch.from_numpy(input)}
+
+        return data
+
+class Normalization(object):
+    def __init__(self, mean = 0.5, std = 0.5):
+        self.mean = mean
+        self.std = std
+    
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        input = (input - self.mean) / self.std
+        
+        data = {'label': label, 'input': input}
+        
+        return data
+
+class RandomFlip(object):
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        if np.random.rand() > 0.5:
+            label = np.fliplr(label)
+            input = np.fliplr(input)
+
+        if np.random.rand() > 0.5:
+            label = np.flipud(label)
+            input = np.flipud(input)
+
+        data = {'label': label, 'input': input}
+
+        return data
+
+## 네트워크 학습하기
